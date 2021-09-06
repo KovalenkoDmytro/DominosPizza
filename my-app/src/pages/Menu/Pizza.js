42c320fab5ff7093776f26f0b5dd6ec08f943332
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-09-06 21:21:30 +0200

    add new change

--- my-app/src/pages/Menu/Pizza.js
+++ my-app/src/pages/Menu/Pizza.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import pizza from '../../data/pizza.json'
-
+import '../Style/Pizza.scss'
 
 function Pizza(props) {
 
@@ -11,9 +11,14 @@ function Pizza(props) {
                 let productName = e.target.parentElement.children[2].innerText;
                 props.addFavorit(productName)
             }}>+add ToFavorit</div>
-            <img className='product__icon' alt={elem.name} src={elem.img} />
-            <span className='product__title'>{elem.name}</span>
-            <span className='product__price'>{elem.price}</span>
+            <img className='product__icon' alt={elem.name} src={elem.img} width="396"  height="396"/>
+            <div className="product__title__wrapper">
+                <span className='product__title'>{elem.name}</span>
+                <span className='product__price'>{elem.price}</span>
+
+            </div>
+
+
             <p className='product__logdescription'>{elem.logdescription}</p>
             <button >+add to basket</button>
         </div>)
@@ -23,12 +28,15 @@ function Pizza(props) {
 
     return (
         <>
-            <div className="content">Pizza</div>
-            <div className="banner">
-                <img src="https://www.dominospizza.pl/DominosPizza/media/Images/modules/menuBanners/mobile/600x240-pizza1.jpg" alt="pizza" />
-                <h1>Pizza</h1>
+            <div className="pizza__page">
+                <div className="content">Pizza</div>
+                <div className="banner">
+                    <img src="https://www.dominospizza.pl/DominosPizza/media/Images/modules/menuBanners/mobile/600x240-pizza1.jpg" alt="pizza" width="1200" height="300" />
+                    <h1>Pizza</h1>
+                </div>
+                <div className="cards">{card}</div>
+
             </div>
-            {card}
         </>
     )
 }
