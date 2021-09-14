d66f1a0cd5b3d96b609f3434db24be0f47c88b0d
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-09-14 22:52:18 +0200

    add new style

--- my-app/src/pages/Menu/Favorits.js
+++ my-app/src/pages/Menu/Favorits.js
@@ -1,11 +1,17 @@
+import close from '../../fonts/icons/close.svg';
+import closeHover from '../../fonts/icons/close_hover.svg';
+import '../Style/Favorits.scss';
+
+
+
 
 function Favorits(props) {
     return (
         <>
-            <div className="content">favorits page</div>
+            <div className="favoritsContent">
             {props.globalStore.map((elem, index) =>
-                <li key={index} >
-                    <img src={elem.img} alt={elem.name} />
+                <li className="favoritProduct" key={index} >
+                    <img src={elem.img} alt={elem.name} height='225' width='225'/>
                     <div className="product__wrapper">
                         <span className="product__name">{elem.name}</span>
                         <span className="product__description">{elem.description}</span>
@@ -13,8 +19,11 @@ function Favorits(props) {
                     <span className="product__price">{elem.price}</span>
                     <button className="del" onClick={(e)=>{
                         props.delProduct(e.target.parentElement.children[1].firstChild.innerText)
-                    }}>X</button>
+                    }}> <img className="close" src={close} alt="close" height='20' width='20'/>
+                    <img className="closeHover" src={closeHover} alt="close" height='20' width='20'/>
+                    </button>
                 </li>)}
+            </div>
         </>
     )
 }
