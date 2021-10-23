68e55eae5c8d7d81045bbc048ced0b12da035b68
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-10-23 12:43:26 +0200

    added favoritsproducts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,11 +9,11 @@ import { useState, useEffect } from 'react';
 function App() {
 
   let [totalPrice, setTotalPrice] = useState([]);
-  let [salePrice, setSalePrice] = useState( []);
+  let [salePrice, setSalePrice] = useState([]);
 
   // let [countProducts, setCountProducts] = useState(0);
 
-  
+
   useEffect(() => {
     if (localStorage.getItem("products") == null) {
       localStorage.setItem("products", '')
@@ -21,17 +21,17 @@ function App() {
       const products = localStorage.getItem("products") || [];
       setTotalPrice(JSON.parse(products))
     }
-    
-    
+
+
     // setCountProducts(
     //   JSON.parse(localStorage.getItem("products")).length
     // )
-   
+
   }, [])
 
 
   useEffect(() => {
-  
+    crossOfPrice();
   }, [salePrice])
 
 
@@ -42,6 +42,13 @@ function App() {
 
   // useEffect(() => {
   // }, [countProducts])
+
+  function crossOfPrice() {
+    if (salePrice.length > 0) {
+      document.querySelector('.total').classList.add('__cossOff');
+    } else return
+  }
+
 
   function addPrice(name, url, price, description, sort) {
     let product = {
@@ -55,10 +62,10 @@ function App() {
     // setCountProducts(totalPrice.length)
   }
 
-  function delProductFromBasket(delProduct){
-    let newProducts =   totalPrice.filter(elem => elem.name !== delProduct );
+  function delProductFromBasket(delProduct) {
+    let newProducts = totalPrice.filter(elem => elem.name !== delProduct);
     setTotalPrice(newProducts)
-}
+  }
 
 
   function returnTotalprice(totalPrice) {
@@ -71,41 +78,39 @@ function App() {
 
 
   // add sale-price and check coupon
-  function changeTotalPrice(saleValue, Pizzas, article ){
+  function changeTotalPrice(saleValue, Pizzas, article) {
     let count = 0
     let countPizzas = 0
     totalPrice.forEach(element => {
       count += element.price;
-      countPizzas+=1;
+      countPizzas += 1;
     });
 
-    let newPrice = count - (count/100) * saleValue;
+    let newPrice = count - (count / 100) * saleValue;
     let SaleCoupon = {
       price: newPrice,
       coupon: article,
       count: Pizzas,
     }
 
-    if(countPizzas >= Pizzas){
-      if(salePrice.length > 0){ 
+    if (countPizzas >= Pizzas) {
+      if (salePrice.length > 0) {
         salePrice.forEach(element => {
-            if(element.coupon === SaleCoupon.coupon){
-              alert('coupon has been activited')
-            }else{
-              setSalePrice([ SaleCoupon])
-            }
+          if (element.coupon === SaleCoupon.coupon) {
+            alert('coupon has been activited')
+          } else {
+            setSalePrice([SaleCoupon])
+          }
         });
-      }
-      else{setSalePrice([ SaleCoupon])}
-    }else(alert(`missing ${Pizzas - countPizzas} pizzas`))
+      } else { setSalePrice([SaleCoupon]) }
+    } else (alert(`missing ${Pizzas - countPizzas} pizzas`))
   }
 
 
   return (
-    <Context.Provider value={[addPrice,changeTotalPrice]} >
-      <Header totalPrice={returnTotalprice(totalPrice)} salePrice={salePrice}  /> 
-      {/* countProducts={countProducts} */}
-      <Navigation  products ={totalPrice} delProduct={delProductFromBasket} />
+    < Context.Provider value={[addPrice, changeTotalPrice]} >
+      <Header totalPrice={returnTotalprice(totalPrice)} salePrice={salePrice} />  { /* countProducts={countProducts} */}
+      <Navigation products={totalPrice} delProduct={delProductFromBasket} />
       <Footer />
     </Context.Provider>
   );
--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,19 +1,20 @@
 import './Styles/Header.scss'
+import { ContextFavorit } from "../Context";
 
+function Header(props) {
 
-function Header(props){
-
-    return(
+    return (
         <>
-        <header>
-            <img className="logo" src="https://www.dominospizza.pl/getmedia/79f93de9-efbb-4c66-b3e6-191159e89dd0/dominos_logo.svg" alt="dominospizza" height='33' width='150'/>    
-            <div className="basket">
-                <div className="total">{props.totalPrice}</div>
-                <div className="salePrice">{props.salePrice[0] === undefined ? "": props.salePrice[0].price.toFixed(2)}</div>
-                {/* <div className="countBasket">{props.countProducts}</div> */}
-            </div>
-        </header> 
+            <header>
+                <img className="logo" src="https://www.dominospizza.pl/getmedia/79f93de9-efbb-4c66-b3e6-191159e89dd0/dominos_logo.svg" alt="dominospizza" height='33' width='150' />
+                <div className="basket">
+                    <div className="total">{props.totalPrice}</div>
+                    <div className="salePrice">{props.salePrice[0] === undefined ? "" : props.salePrice[0].price.toFixed(2)}</div>
+                    {/* <div className="countBasket">{props.countProducts}</div> */}
+                </div>
+                <div className="favoritsProducts"></div>
+            </header>
         </>
     )
 }
- export default Header;
+export default Header;
