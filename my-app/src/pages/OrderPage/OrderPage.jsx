048b43f4384f47471ea507696f36477ab8b06c6d
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-11-19 23:51:27 +0100

    added syle for order page

--- my-app/src/pages/OrderPage/OrderPage.jsx
+++ my-app/src/pages/OrderPage/OrderPage.jsx
@@ -1,70 +1,74 @@
+import "./OrderPage.scss"
 
-
-function OrderPage(props){
+function OrderPage(props) {
 
     let productsOrdered = props.products;
-    
+
     //get time
-    function getTime(min){
+    function getTime(min) {
         let nowTime = new Date()
-        nowTime.setMinutes(nowTime.getMinutes()+min)
+        nowTime.setMinutes(nowTime.getMinutes() + min)
 
         let hours = nowTime.getHours()
         let minutes = nowTime.getMinutes()
-        if (minutes < 10){
+        if (minutes < 10) {
             minutes = `0${nowTime.getMinutes()}`
         }
-        if (hours < 10){
+        if (hours < 10) {
             hours = `0${nowTime.getHours()}`
         }
 
-        return  `${hours}:${minutes}`
+        return `${hours}:${minutes}`
     };
 
-    
-    return(
+
+    return (
         <>
-        <div className="takeInform">
-            <div className="takeInform_top">
-                <span>Dostawa</span>
-                <span>Odbiór osobisty</span>
-            </div>
-            <div className="takeInform_content">
-                <div className="content_item">
-                    <img src="https://www.dominospizza.pl/getmedia/01a4fbdd-c165-40c9-882a-62ac87715f37/dominos_skuter.gif.aspx" alt="delivery" />
-                    <span>Przybliżony czas dostawy:</span>
-                    {getTime(25)}
+            <div className="order_page">
+                <div className="order_page_top">
+                    <img className="background" src="https://www.dominospizza.pl/DominosPizza/media/Images/modules/cartconfiguration/bg_1200x471.jpg" alt="dominos" />
+                    <div className="takeInform">
+                        <div className="takeInform_top">
+                            <span>Dostawa</span>
+                            <span>Odbiór osobisty</span>
+                        </div>
+                        <div className="takeInform_content">
+                            <div className="content_item">
+                                <img src="https://www.dominospizza.pl/getmedia/01a4fbdd-c165-40c9-882a-62ac87715f37/dominos_skuter.gif.aspx" alt="delivery" />
+                                <span>Przybliżony czas dostawy:</span>
+                                <span className="item_time">{getTime(25)}</span>
+                            </div>
+
+                            <div className="content_item">
+                                <img src="https://www.dominospizza.pl/getmedia/d6c4e4a2-f52a-45a1-b3bc-c7da00e4f58f/odbior_osobisty.gif.aspx" alt="takeAway" />
+                                <span>Przybliżony czas odbioru:</span>
+                                <span className="item_time">{getTime(15)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="takeContent">
+                    <div className="content_item">
+
+                    </div>
+                    <div className="recapitulation">
+                        <span>Podsumowanie</span>
+                        <ul className="listProducts">
+                            {productsOrdered.map((currentValue, index) =>
+                                <li key={index} className='choosedProduct'>
+                                    <span> {currentValue.name}</span>
+                                    <span>{currentValue.description}</span>
+                                </li>)
+                            }
+                        </ul>
+                        <div className="total">
+
+                        </div>
+                    </div>
                 </div>
 
-                <div className="content_item">
-                    <img src="https://www.dominospizza.pl/getmedia/d6c4e4a2-f52a-45a1-b3bc-c7da00e4f58f/odbior_osobisty.gif.aspx" alt="takeAway" />
-                    <span>Przybliżony czas odbioru:</span>
-                    {getTime(15)}
-                </div>
+                <p>Twoje zamowienie to</p>
             </div>
-        </div>   
-        <div className="takeContent">
-            <div className="content_item">
-
-            </div>
-            <div className="recapitulation">
-                <span>Podsumowanie</span>
-                <ul className="listProducts">
-                    {productsOrdered.map((currentValue, index)=>
-                        <li key={index} className='choosedProduct'>
-                           <span> {currentValue.name}</span>
-                           <span>{currentValue.description}</span>
-                        </li>)
-                    }    
-                </ul>
-                <div className="total">
-                    
-                </div>
-            </div>
-        </div>
-
-         <p>Twoje zamowienie to</p>
-        
         </>
     )
 
