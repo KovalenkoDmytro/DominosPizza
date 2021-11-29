65c7f6a5d998e6c684e69cd39db7bde9e97af639
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-11-29 21:02:20 +0100

    added style and switch exange ship way

--- my-app/src/pages/OrderPage/OrderPage.jsx
+++ my-app/src/pages/OrderPage/OrderPage.jsx
@@ -16,7 +16,19 @@ function OrderPage(props) {
     }, []);
 
     useEffect(() => {
-        console.log(delivery);
+        if (delivery) {
+            document.querySelector('.takeInform_top .delivery').classList.add('active');
+            document.querySelector('.takeInform_top .takeAway').classList.remove('active');
+            document.querySelector('.takeInform_content img[alt="delivery"]').classList.add('active');
+            document.querySelector('.takeInform_content img[alt="takeAway"]').classList.remove('active');
+        } else {
+            document.querySelector('.takeInform_top .delivery').classList.remove('active');
+            document.querySelector('.takeInform_top .takeAway').classList.add('active');
+            document.querySelector('.takeInform_content img[alt="delivery"]').classList.remove('active');
+            document.querySelector('.takeInform_content img[alt="takeAway"]').classList.add('active');
+        }
+
+
     }, [delivery])
 
 
@@ -45,21 +57,21 @@ function OrderPage(props) {
                     <img className="background" src="https://www.dominospizza.pl/DominosPizza/media/Images/modules/cartconfiguration/bg_1200x471.jpg" alt="dominos" />
                     <div className="takeInform">
                         <div className="takeInform_top">
-                            <span onClick={() => {
+                            <span className="delivery" onClick={() => {
                                 setDelivery(true)
                             }}>Dostawa</span>
-                            <span onClick={() => {
+                            <span className="takeAway" onClick={() => {
                                 setDelivery(false)
                             }}>Odbiór osobisty</span>
                         </div>
-                        <div className="takeInform_content">
-                            <div className="content_item">
+                        <div className="takeInform_content" >
+                            <div className="content_item"onClick={() => {setDelivery(true)}} >
                                 <img src="https://www.dominospizza.pl/getmedia/01a4fbdd-c165-40c9-882a-62ac87715f37/dominos_skuter.gif.aspx" alt="delivery" />
                                 <span>Przybliżony czas dostawy:</span>
                                 <span className="item_time">{getTime(25)}</span>
                             </div>
 
-                            <div className="content_item">
+                            <div className="content_item" onClick={() => {setDelivery(false)}}>
                                 <img src="https://www.dominospizza.pl/getmedia/d6c4e4a2-f52a-45a1-b3bc-c7da00e4f58f/odbior_osobisty.gif.aspx" alt="takeAway" />
                                 <span>Przybliżony czas odbioru:</span>
                                 <span className="item_time">{getTime(15)}</span>
@@ -101,27 +113,27 @@ function OrderPage(props) {
                 <div className="orderChoose">
                     <div className="formTitle">Płatność</div>
                     <form action="">
-                        <select name="pay" id="pay">
-                            <option value="Gotówka">Gotówka</option>
-                            <option value="Karta (przy odbiorze)">Karta (przy odbiorze) </option>
-                            <option value="Online">Online</option>
+                        <select className="selects" name="pay" id="pay">
+                            <option value="cash">Gotówka</option>
+                            <option value="card">Karta (przy odbiorze) </option>
+                            <option value="online">Online</option>
                         </select>
                         <label htmlFor="phone">Telefon*</label>
-                        <input type="tel" name="phone" id="phone" required/>
+                        <input className="inputs" type="tel" name="phone" id="phone" required />
                         <label htmlFor="email">Email*</label>
-                        <input type="email" name="email" id="email" required/>
+                        <input className="inputs" type="email" name="email" id="email" required />
                         <div className="person">
                             <label htmlFor="persone">Imię</label>
-                            <input type="text" name="persone" id="persone" required/>
+                            <input className="inputs" type="text" name="persone" id="persone" required />
                         </div>
                         <p>Administratorem Twoich danych osobowych jest DP Polska S.A. z siedzibą w Warszawie, ul. Dąbrowieckiej 30.</p>
                         <div className="privatePolitic">
-                          <input type="checkbox" name="privatePolitic" id="privatePolitic" required/>  
-                          <span>Akceptuję <a href="https://www.dominospizza.pl/getmedia/0846bb39-0f3f-4a8b-a858-831deb55035e/Regulamin_WWW_Dominos_Pizza.pdf" target="_blank">regulamin</a> serwisu Domino's Pizza </span>
-                        </div>  
-                        <button className="solid">Zamawiam z obowiązkiem zapłaty</button>    
+                            <input type="checkbox" name="privatePolitic" id="privatePolitic" required />
+                            <span>Akceptuję <a href="https://www.dominospizza.pl/getmedia/0846bb39-0f3f-4a8b-a858-831deb55035e/Regulamin_WWW_Dominos_Pizza.pdf" target="_blank">regulamin</a> serwisu Domino's Pizza </span>
+                        </div>
+                        <button className="solid">Zamawiam z obowiązkiem zapłaty</button>
                     </form>
-                </div>                
+                </div>
             </div>
         </>
     )
