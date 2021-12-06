a20bc7df864546fbd1c567759b7b4f63e10046b1
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-12-06 22:09:34 +0100

    Update TimePicker.jsx

--- my-app/src/pages/TimePicker/TimePicker.jsx
+++ my-app/src/pages/TimePicker/TimePicker.jsx
@@ -8,32 +8,44 @@ function TimePicker(props) {
     const hoursInput = React.createRef();
     const minutesInput = React.createRef();
 
-    function incrementTime(timeItem){
-        return timeItem.current.value = Number(timeItem.current.value) + 1 
+    function incrementTime(timeItem, num){
+        let newValue = Number(timeItem.current.value) + 1 
+        if(newValue > num){
+            newValue = 0
+        }
+        return timeItem.current.value = newValue
     };
-    function decrementTime(timeItem){
-        return timeItem.current.value = Number(timeItem.current.value) - 1 
+    function decrementTime(timeItem, num){
+        let newValue = Number(timeItem.current.value) - 1 
+        if(newValue < 0){
+            newValue = num
+        }
+        return timeItem.current.value = newValue
     };
+
+    
 
     return (
         <>
             <form action="">
                 <div className="hours">
                     <input className="hoursIncrem" type="button" value="+" onClick={()=>{
-                        incrementTime(hoursInput)
+                        incrementTime(hoursInput ,23)
                     }}/>
-                    <input type="text" ref={hoursInput} value={new Date().getHours()}/>
+                    <input type="text" ref={hoursInput} value={new Date().getHours()} onChange={(e)=>{
+                        console.log(e.target);
+                    }}/>
                     <input className="hoursDecrem" type="button" value="-" onClick={()=>{
-                        decrementTime(hoursInput)
+                        decrementTime(hoursInput,23)
                     }}/>
                 </div>
                 <div className="seconds">
                     <input className="secondsIncrem" type="button" value="+" onClick={()=>{
-                        incrementTime(minutesInput)
+                        incrementTime(minutesInput, 59)
                     }}/>
                     <input type="text" ref={minutesInput} value={new Date().getMinutes()}/>
                     <input className="secondsDecrem" type="button" value="-" onClick={()=>{
-                        decrementTime(minutesInput)
+                        decrementTime(minutesInput ,59)
                     }}/>
                 </div>
                 <button type="submit">Potwierdzam</button>
